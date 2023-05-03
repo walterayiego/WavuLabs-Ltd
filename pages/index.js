0b89920a7d3fcac5df986f67f5755c3a4694b3de
Walter Ayiego <walterayiego@gmail.com>
2023-05-03 13:02:47 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -21,8 +21,26 @@ export default function Home() {
         to startups and enterprises. We are a team of software engineers, designers, and project managers. 
         We build custom software for startups and enterprises. 
           "
+          about="WavuLabs is a software development company that provides custom IT solutions 
+        to startups and enterprises. We are a team of software engineers, designers, and project managers. 
+        We build custom software for startups and enterprises. "
+          keywords="Software Development, Software Development Company, 
+          Software Development Company in Kenya, Software Development Company in Nakuru, 
+          Software Development Company in Africa, Software Development Company in Nairobi,
+          Mobile App Development, Mobile App Development Company, Mobile App Development Company in Kenya,
+          Mobile App Development Company in Nakuru, Mobile App Development Company in Africa,
+          Web Development, Web Development Company, Web Development Company in Kenya,
+          Website Development, Website Development Company, Website Development Company in Kenya,
+          WavuLabs, WavuLabs Company, WavuLabs Company in Kenya, WavuLabs Company in Nakuru,
+          Wavu Labs Company in Africa, Wavu Labs Company in Nairobi, Wavu Labs Company in Nakuru,
+          
+           "
         />
-        <link rel="icon" href="https://i.ibb.co/Fgf5z6b/Transparent.png"  type="image/x-icon" />
+        <link
+          rel="icon"
+          href="https://i.ibb.co/Fgf5z6b/Transparent.png"
+          type="image/x-icon"
+        />
       </Head>
       <Main />
       <Services />
